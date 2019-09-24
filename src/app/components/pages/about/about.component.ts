a555d4bd2b6f25c0cdcc138d896a99c3784240dc
edgon <edgon85@gmail.com>
2019-09-24 12:57:03 -0600

    agregano informacion de telefonos, correos y link con interpolacion en html

--- src/app/components/pages/about/about.component.ts
+++ src/app/components/pages/about/about.component.ts
@@ -7,6 +7,8 @@ import { Component, OnInit } from '@angular/core';
 })
 export class AboutComponent implements OnInit {
 
+  imagesPath = 'assets/pages/images/about/';
+
   constructor() { }
 
   ngOnInit() {
--- src/app/components/pages/contact/contact.component.ts
+++ src/app/components/pages/contact/contact.component.ts
@@ -7,6 +7,13 @@ import { Component, OnInit } from '@angular/core';
 })
 export class ContactComponent implements OnInit {
 
+  email1 = 'contacto@capingt.com';
+  phone1 = '42768937';
+  phone2 = '59483746';
+  facebookUrl = 'https://www.facebook.com/CAP-IN-Capacitaci%C3%B3n-y-Asesor%C3%ADa-Profesional-e-Innovadora-2378216642421046/';
+  twitterUrl = '#';
+  instagramUrl = '#';
+
   constructor() { }
 
   ngOnInit() {
