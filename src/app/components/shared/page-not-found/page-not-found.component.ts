b71bdd5d1b271163dc256d61de6c170c11ca5ccc
edgon <edgon85@gmail.com>
2019-09-29 03:33:02 -0600

    agregando diseño al template de page not found

--- src/app/components/shared/page-not-found/page-not-found.component.ts
+++ src/app/components/shared/page-not-found/page-not-found.component.ts
@@ -7,6 +7,8 @@ import { Component, OnInit } from '@angular/core';
 })
 export class PageNotFoundComponent implements OnInit {
 
+  year = new Date().getFullYear();
+
   constructor() { }
 
   ngOnInit() {
