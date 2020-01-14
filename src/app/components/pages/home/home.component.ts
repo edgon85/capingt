7c8bf1716586318bcb152bc5a4a5f4c576cd1298
edgon <edgon85@gmail.com>
2020-01-14 13:11:41 -0600

    obteniendo promociones desde firebase

--- src/app/components/pages/home/home.component.ts
+++ src/app/components/pages/home/home.component.ts
@@ -7,22 +7,24 @@ import { MessagesService } from '../../../services/admin/messages.service';
   styleUrls: ['./home.component.css']
 })
 export class HomeComponent implements OnInit {
-
   pathImage = 'assets/pages/images/home/';
   _altImage = 'cap in capacitación y asesoria';
 
-  promoDate = false;
+  promoDate = true;
+  titlePromo = '';
+  imageUrl = '';
+  textPromo = '';
 
-  constructor( private _settingsService: MessagesService) {}
+  constructor(private _settingsService: MessagesService) {}
 
   ngOnInit() {
-    this._settingsService.getEvento().subscribe(
-      (resp: any) => {
-        // tslint:disable-next-line:prefer-const
-        let datos = resp.payload.data();
-        this.promoDate = datos.evento;
-      }
-    );
+    this._settingsService.getEvento().subscribe((resp: any) => {
+      // tslint:disable-next-line:prefer-const
+      let datos = resp.payload.data();
+      this.promoDate = datos.evento;
+      this.titlePromo = datos.title;
+      this.imageUrl = datos.imagen;
+      this.textPromo = datos.text;
+    });
   }
-
 }
